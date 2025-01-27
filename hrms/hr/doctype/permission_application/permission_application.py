a0dcd47e2cca8067f7aad1cb54acc38c8b459de9
kalisakelly <kalisakelly2001@gmail.com>
2025-01-27 15:19:12 +0200

    modifying permission

--- hrms/hr/doctype/permission_application/permission_application.py
+++ hrms/hr/doctype/permission_application/permission_application.py
@@ -5,14 +5,24 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import nowdate
 
+from frappe.model.document import Document
+from frappe.utils import nowdate, get_datetime
+from datetime import datetime
+
 class PermissionApplication(Document):
     def validate(self):
-        # self.validate_time()
-        pass
+        self.validate_time()
 
-    # def validate_time(self):
-    #     # Ensure arriving time is not less than leaving time
-    #     if self.date_and_time_of_arriving < self.date_and_time_of_leaving:
-    #         frappe.throw("The date and time of arriving cannot be before the date and time of leaving.")
-    #     # if self.date_and_time_of_leaving< :
-    #     #     frappe.throw("the date can't be in the past.")
+    def validate_time(self):
+        # Convert fields to datetime objects
+        date_and_time_of_arriving = get_datetime(self.date_and_time_of_arriving)
+        date_and_time_of_leaving = get_datetime(self.date_and_time_of_leaving)
+        current_date = get_datetime(nowdate())
+
+        # Ensure arriving time is not less than leaving time
+        if date_and_time_of_arriving < date_and_time_of_leaving:
+            frappe.throw("The date and time of arriving cannot be before the date and time of leaving.")
+
+        # Ensure leaving time is not in the past
+        if date_and_time_of_leaving < current_date:
+            frappe.throw("The date and time of leaving cannot be in the past.")
