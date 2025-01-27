5544ded9f20b85033339f552c77551a7e44ec271
kalisakelly <kalisakelly2001@gmail.com>
2025-01-27 14:49:33 +0200

    modifying permission

--- hrms/hr/doctype/permission_application/permission_application.py
+++ hrms/hr/doctype/permission_application/permission_application.py
@@ -13,5 +13,5 @@ class PermissionApplication(Document):
         # Ensure arriving time is not less than leaving time
         if self.date_and_time_of_arriving < self.date_and_time_of_leaving:
             frappe.throw("The date and time of arriving cannot be before the date and time of leaving.")
-        if self.date_and_time_of_leaving < nowdate():
-            frappe.throw("the date can't be in the past.")
+        # if self.date_and_time_of_leaving< :
+        #     frappe.throw("the date can't be in the past.")
